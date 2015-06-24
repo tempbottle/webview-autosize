ed674258ca040495e1251578c097da8519457a4e
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-06-24 13:11:32 +0300

    Create the example

---
+++ README.md
@@ -0,0 +1,4 @@
+#NativeScript 1.1.0 webView auto-size
+
+Due to the specifics of the iOS measurements, the WebView component does not auto-size in iOS (NativeScript version 1.1.0).
+This sample demonstrates how this can be achieved.
---
+++ app/main-view-model.js
@@ -0,0 +1,28 @@
+var observable = require("data/observable");
+var platformModule = require("platform");
+var DemoModel = (function (_super) {
+    __extends(DemoModel, _super);
+    function DemoModel() {
+        _super.call(this);
+        this.set("webViewUrl", "~/myPage.html");
+        if (platformModule.device.os == "iOS") {
+        this.set("webViewHeight", 100);
+        this.set("webViewWidth", 100);
+        } else {
+        this.set("webViewHeight", "");
+        this.set("webViewWidth", "");
+        }
+    }
+
+    DemoModel.prototype.loadFinished = function(args) {
+        if (platformModule.device.os == "iOS") {
+//            args.object.requestLayout();
+        this.set("webViewHeight", "");
+        this.set("webViewWidth", "");
+        }
+        console.log("Load Finished");
+    }
+    return DemoModel;
+})(observable.Observable);
+exports.DemoModel = DemoModel;
+exports.mainViewModel = new DemoModel();
