d5e77375fb83f0c27ca0c8b00c955952924bbb8f
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-07-06 11:24:51 +0300

    Trace enabled

--- app/app.js
+++ app/app.js
@@ -1,4 +1,8 @@
 var application = require("application");
+var trace = require("trace");
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
+
+trace.enable();
+trace.addCategories("Layout");
 application.start();
