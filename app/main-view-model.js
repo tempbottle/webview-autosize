e92de992f40b955d8da3d0c7c1e7c35ac29b7702
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-07-06 11:29:15 +0300

    Fix with minWidth/minHeight + requestLayout call

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -6,19 +6,19 @@ var DemoModel = (function (_super) {
         _super.call(this);
         this.set("webViewUrl", "~/myPage.html");
         if (platformModule.device.os == "iOS") {
-            this.set("webViewHeight", 100);
-            this.set("webViewWidth", 100);
+//            this.set("webViewHeight", 100);
+//            this.set("webViewWidth", 100);
         } else {
-            this.set("webViewHeight", "");
-            this.set("webViewWidth", "");
+//            this.set("webViewHeight", "");
+//            this.set("webViewWidth", "");
         }
     }
 
     DemoModel.prototype.loadFinished = function(args) {
         if (platformModule.device.os == "iOS") {
-//            args.object.requestLayout();
-        this.set("webViewHeight", "");
-        this.set("webViewWidth", "");
+            args.object.requestLayout();
+//        this.set("webViewHeight", "");
+//        this.set("webViewWidth", "");
         }
         console.log("Load Finished");
     }
